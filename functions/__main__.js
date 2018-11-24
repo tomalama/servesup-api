4d2cba58f71a484455d531f62ebcfca07810ec1b
tomalama <tommy.nguyen7@hotmail.com>
2018-11-24 00:48:12 -0500

    Initial commit

---
+++ functions/__main__.js
@@ -0,0 +1,10 @@
+const axios = require("axios");
+
+/**
+ * A basic Hello World function
+ * @param {string} name Who you're saying hello to
+ * @returns {string}
+ */
+module.exports = (name = "world", context, callback) => {
+  callback(null, `hello ${name}`);
+};
