ced6f577f70be8a0b5f475b3c30c878ff113d5c6
0Calories <cppanand@gmail.com>
2018-11-24 03:23:49 -0500

    Include type param

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const constants = require('../src/constants/constants');
 
-const location = '-33.8670522,151.1957362';
+const location = '43.004525099999995,-81.27643669999999';
 const radius = '1500';
 
 const getRestaurants = async (location, radius, key) => {
@@ -10,14 +10,15 @@ const getRestaurants = async (location, radius, key) => {
             params: {
                 location,
                 radius,
-                key
+                key,
+                type: 'restaurant'
             }
         });
         console.log(data.results);
     } catch (error) {
         console.log(error);
     }
-}
+};
 
 getRestaurants(location, radius, constants.API_KEY);
 
