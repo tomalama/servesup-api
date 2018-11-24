757ecf1eacddcfc3ee43f0ba3db624219720856e
tomalama <tommy.nguyen7@hotmail.com>
2018-11-24 14:07:10 -0500

    Finished API functionality

--- functions/__main__.js
+++ functions/__main__.js
@@ -1,15 +1,21 @@
 // Import libraries
 const axios = require("axios");
-
-const { storeRestaurantMenu, getRestaurantMenu} = require("./storage");
+const lib = require("lib")({
+  token: process.env.STDLIB_LIBRARY_TOKEN
+});
+const storage = lib.utils.storage["@0.1.6"];
+const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY;
 
 /**
- * A function that gives you nutritional information about dishes around the location.
+ * A function that gives you nutritional information about restaurant menu items around the location.
  * @param {string} ll Latitude, longitude
- * @param {string} distance Radius around location to search
+ * @param {string} distance Radius around location to search in meters
  * @returns {string}
  */
-module.exports = async (ll = "43.0044943,-81.2764182", distance = "50m") => {
+module.exports = async (
+  ll = "43.004525099999995,-81.27643669999999",
+  distance = "300"
+) => {
   const instance = axios.create({
     baseURL: "https://trackapi.nutritionix.com/v2/",
     timeout: 3000,
@@ -20,64 +26,131 @@ module.exports = async (ll = "43.0044943,-81.2764182", distance = "50m") => {
   });
 
   // Gets information about restaurants around the coordinates
-  let locationRequest = await instance.get("locations", {
-    params: {
-      ll,
-      distance
-    }
-  });
-  let { locations } = locationRequest.data;
+  let locationRequest = await getRestaurantsByNutritionix(
+    instance,
+    ll,
+    distance
+  );
+
+  // If the first API call did not query, query the locations an alternative way
+  if (!locationRequest) {
+    console.log(
+      "Couldn't get locations from Nutritionix API. Trying Google Maps."
+    );
+    locationRequest = await getRestaurantsByMaps(ll, distance);
+  }
+
+  if (!locationRequest) {
+    return "Could not query any locations through Google Maps. Quitting.";
+  }
+
+  // Get locations based on input
+  const { data } = locationRequest;
+  const locations = data.locations || data.results;
 
   // Store an array for all menu items (in all locations)
   let allMenuItems = [];
 
   // Iterate through each location
-  locations.forEach(async location => {
+  for (let location of locations) {
     // Check if menu for restaurant is already stored or not before performing an API request
-    const menu = await retrieveRestaurantMenu(location.brand_name);
+    const menu = await storage.get(location.name);
     if (menu) {
-      // Get menu here
+      // Push menu into allMenuItems array
       for (let item in menu) {
         allMenuItems.push({ nix_item_id: menu[item] });
       }
     } else {
+      // Build common params
+      let params = {
+        query: location.name
+      };
+
+      // Depending on where the input came from, there is extra brand_ids info
+      if (location.brand_id) {
+        params.brand_ids = [location.brand_id];
+      }
+
       // Request menu items for location
-      let menuForLocationRequest = await instance.get("search/instant", {
-        params: {
-          brand_ids: [location.brand_id],
-          query: location.brand_name
-        }
+      const menuForLocationRequest = await instance.get("search/instant", {
+        params
       });
-      let { branded } = menuForLocationRequest.data;
+      const { branded } = menuForLocationRequest.data;
 
-      // TODO: store {brand_name, branded}
-      storeRestaurantMenu(location.brand_name, branded.map((menuItem) => menuItem.nix_item_id));
+      // Check if menu is empty
+      if (branded.length > 0) {
+        // Add menu items for this location into allMenuItems array
+        allMenuItems = allMenuItems.concat(branded);
 
-      // Add menu items for this location into allMenuItems array
-      allMenuItems.concat(branded);
+        // Store the store name as the key and an array of menu items as the value
+        await storage.set(
+          location.name,
+          branded.map(menuItem => menuItem.nix_item_id)
+        );
+      }
     }
-  });
+  }
 
   // Get nutritional information for all menu items
   const allMenuItemsWithNutritions = await Promise.all(
-    allMenuItems.map(async (menuItem, index) => {
-      const nutritionInfo = await retreiveMenuItem(menuItem.nix_item_id);
+    // Iterate through the menu items
+    allMenuItems.map(async menuItem => {
+      // Get the nutritionInfo if it exists in the storage
+      const nutritionInfo = await storage.get(menuItem.nix_item_id);
+
+      // Return nutritionInfo if exists
       if (nutritionInfo) {
         return nutritionInfo;
       }
 
-      let item = await instance.get("search/item", {
+      // Otherwise, query it if it doesn't exist
+      let menuItemRequest = await instance.get("search/item", {
         params: {
           nix_item_id: menuItem.nix_item_id,
           claims: true
         }
       });
 
-      return item.data.foods[0];
+      // Store the item in the storage
+      let item = menuItemRequest.data.foods[0];
+      await storage.set(menuItem.nix_item_id, item);
+
+      return item;
     })
   );
 
   console.log(allMenuItemsWithNutritions);
 
-  return "Sup lol"; //JSON.stringify(allMenuItemsWithNutritions);
+  return JSON.stringify(allMenuItemsWithNutritions);
+};
+
+const getRestaurantsByNutritionix = async (instance, ll, distance) => {
+  try {
+    const request = await instance.get("locations", {
+      params: {
+        ll,
+        distance: distance.concat("", "m")
+      }
+    });
+    return request;
+  } catch (error) {}
+};
+
+const getRestaurantsByMaps = async (location, radius) => {
+  try {
+    const request = await axios.get(
+      `https://maps.googleapis.com/maps/api/place/nearbysearch/json`,
+      {
+        params: {
+          location,
+          radius,
+          key: GOOGLE_MAPS_API_KEY,
+          type: "restaurant"
+        }
+      }
+    );
+    return request;
+  } catch (error) {
+    console.log(error);
+  }
 };
